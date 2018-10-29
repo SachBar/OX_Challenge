bf3c98da8efef254d66fc9786b2cca99d3c3fe73
Your Name <you@example.com>
2018-10-29 19:22:08 +0100

    Envois des massages en asynchrone

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import jsonify , render_template
+from flask import jsonify, render_template
 
 from pandas import DataFrame, read_csv
 import matplotlib.pyplot as plt
@@ -9,27 +9,28 @@ data = 'data/final.csv'
 
 global bool
 
+
 @app.route('/')
 @app.route('/index')
 @app.route('/graph_example')
 def graph_example():
     return render_template('index.html')
 
+
 @app.route('/test')
 def test():
     global bool
     if bool == 0:
-        response = {'discount': False, "timeRemaining" : 60}
+        response = {'discount': False, "timeRemaining": 60}
     else:
-        response = {'discount': True, "timeRemaining" : 60}
+        response = {'discount': True, "timeRemaining": 60}
 
     return jsonify(response)
 
 
-
-
 @app.route('/value/<x>')
 def value(x):
+    print(x)
     df = pd.read_csv(data)
     global bool
     if df.iloc[int(x)-1, 2] - df.iloc[int(x)-1, 1] > 0:
